ef7e3efe6ff3352caf71180d8c2286761533dd9d
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2025-03-23 14:36:26 -0700

    Nav links

--- root/.vitepress/config.mjs
+++ root/.vitepress/config.mjs
@@ -19,8 +19,8 @@ export default defineConfig({
       { text: 'About', link: '/about' },
       { text: 'Awards', link: '/awards' },
       { text: 'Contact', link: 'mailto:contact@westcoastinvitational.org' },
-      { text: 'Register', link: 'https://forms.gle/BikYtwEAd88kv4hr8' },
-      { text: 'Volunteer', link: 'https://forms.gle/mhn4rXsb4iEnrCjW8' }
+      { text: 'Register', link: '/apply' },
+      { text: 'Volunteer', link: '/volunteer' }
     ],
 
     sidebar: [
