9b0ce2de2b3e36ee0c18224ee0c33a301b227b5a
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2025-12-17 08:48:06 -0800

    Add Planning Board link

--- root/public/constants.js
+++ root/public/constants.js
@@ -1,3 +1,4 @@
 const APPLICATION_FORM = "https://forms.gle/qUNGz1uPeZLpQZRt9"
 const VOLUNTEER_FORM = "https://forms.gle/ppDmqBTJwELmM8jN8"
 const DISCORD = "https://discord.com/invite/JmVjbpxUj3"
+const BOARD_FORM = "https://forms.gle/7sq3MzkY4tNCQMsN7"
